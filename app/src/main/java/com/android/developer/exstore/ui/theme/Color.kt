2a0693b56127d7af050be0d4a5fa32ce073f1dfe
isekaiweb <cobasaja271@gmail.com>
2022-09-09 19:31:13 +0700

    :tada: init project

---
+++ app/src/main/java/com/android/developer/exstore/ui/theme/Color.kt
@@ -0,0 +1,46 @@
+package com.android.developer.exstore.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val Blue10 = Color(0xFF020931)
+val Blue20 = Color(0xFF041262)
+val Blue30 = Color(0xFF061b93)
+val Blue40 = Color(0xFF0824c4)
+val Blue80 = Color(0xFF9dabfb)
+val Blue90 = Color(0xFFced5fd)
+
+
+val DarkBlue10 = Color(0xFF000033)
+val DarkBlue20 = Color(0xFF000066)
+val DarkBlue30 = Color(0xFF000099)
+val DarkBlue40 = Color(0xFF0000cc)
+val DarkBlue80 = Color(0xFF9999ff)
+val DarkBlue90 = Color(0xFFccccff)
+
+val Cyan10 = Color(0xFF003333)
+val Cyan20 = Color(0xFF006666)
+val Cyan30 = Color(0xFF009999)
+val Cyan40 = Color(0xFF00cccc)
+val Cyan80 = Color(0xFF99ffff)
+val Cyan90 = Color(0xFFccffff)
+
+val Red10 = Color(0xFF410001)
+val Red20 = Color(0xFF680003)
+val Red30 = Color(0xFF930006)
+val Red40 = Color(0xFFBA1B1B)
+val Red80 = Color(0xFFFFB4A9)
+val Red90 = Color(0xFFFFDAD4)
+
+val Grey10 = Color(0xFF191C1D)
+val Grey20 = Color(0xFF2D3132)
+val Grey80 = Color(0xFFC4C7C7)
+val Grey90 = Color(0xFFE0E3E3)
+val Grey95 = Color(0xFFEFF1F1)
+val Grey99 = Color(0xFFFBFDFD)
+
+val BlueGrey30 = Color(0xFF818abb)
+val BlueGrey50 = Color(0xFF6e7ccf)
+val BlueGrey60 = Color(0xFF6475d8)
+val BlueGrey80 = Color(0xFF5168ec)
+val BlueGrey90 = Color(0xFF4761f5)
---
+++ app/src/main/java/com/android/developer/exstore/ui/theme/Theme.kt
@@ -0,0 +1,83 @@
+package com.android.developer.exstore.ui.theme
+
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+
+private val DarkColorPalette = darkColorScheme(
+    primary = Blue80,
+    onPrimary = Blue20,
+    primaryContainer = Blue30,
+    onPrimaryContainer = Blue90,
+    inversePrimary = Blue40,
+    secondary = DarkBlue80,
+    onSecondary = DarkBlue20,
+    secondaryContainer = DarkBlue30,
+    onSecondaryContainer = DarkBlue90,
+    tertiary = Cyan80,
+    onTertiary = Cyan20,
+    tertiaryContainer = Cyan30,
+    onTertiaryContainer = Cyan90,
+    error = Red80,
+    onError = Red20,
+    errorContainer = Red30,
+    onErrorContainer = Red90,
+    surface = Grey10,
+    onSurface = Grey80,
+    inverseSurface = Grey90,
+    inverseOnSurface = Grey20,
+    surfaceVariant = BlueGrey30,
+    onSurfaceVariant = BlueGrey80,
+    outline = BlueGrey60
+
+)
+
+private val LightColorPalette = lightColorScheme(
+    primary = Blue40,
+    onPrimary = Color.White,
+    primaryContainer = Blue90,
+    onPrimaryContainer = Blue10,
+    inversePrimary = Blue80,
+    secondary = DarkBlue40,
+    onSecondary = Color.White,
+    secondaryContainer = DarkBlue90,
+    onSecondaryContainer = DarkBlue10,
+    tertiary = Cyan40,
+    onTertiary = Color.White,
+    tertiaryContainer = Cyan90,
+    onTertiaryContainer = Cyan10,
+    error = Red40,
+    onError = Color.White,
+    errorContainer = Red90,
+    onErrorContainer = Red10,
+    background = Grey99,
+    onBackground = Grey10,
+    surface = Grey99,
+    onSurface = Grey10,
+    inverseSurface = Grey20,
+    inverseOnSurface = Grey95,
+    surfaceVariant = BlueGrey90,
+    onSurfaceVariant = BlueGrey30,
+    outline = BlueGrey50
+)
+
+@Composable
+fun ExStoreTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val useDynamicColors = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    val colors = when {
+        useDynamicColors && darkTheme -> dynamicDarkColorScheme(LocalContext.current)
+        useDynamicColors && !darkTheme -> dynamicLightColorScheme(LocalContext.current)
+        darkTheme -> DarkColorPalette
+        else -> LightColorPalette
+    }
+
+    MaterialTheme(
+        colorScheme = colors,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
