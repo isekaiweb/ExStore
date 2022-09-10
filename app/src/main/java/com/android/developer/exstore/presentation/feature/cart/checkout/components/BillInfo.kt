bf92e0f8b66407f748c4da90dce129bbe91d501f
isekaiweb <cobasaja271@gmail.com>
2022-09-11 03:14:26 +0700

    :fire: remove unused import

--- app/src/main/java/com/android/developer/exstore/presentation/feature/cart/checkout/components/BillInfo.kt
+++ app/src/main/java/com/android/developer/exstore/presentation/feature/cart/checkout/components/BillInfo.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.android.developer.exstore.presentation.util.toReadAble
 
 @Composable
